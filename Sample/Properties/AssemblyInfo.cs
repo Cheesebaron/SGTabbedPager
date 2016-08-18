e1e21218ed0acc4a1073597ee4d9d2439d261042
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-08-18 19:17:04 +0200

    Move sample

--- Sample/Properties/AssemblyInfo.cs
+++ Sample/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,6 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
+
+[assembly: AssemblyInformationalVersion("0.0.2+Branch.master.Sha.a58ff3f2dcd2331ca2541440630f62beb46262a5")]
